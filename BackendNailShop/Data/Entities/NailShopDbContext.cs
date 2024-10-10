b40702e2f608f91f4c39b96726c9e0f28ef132f4
thav2003 <vuthase172485@fpt.edu.vn>
2024-10-10 17:50:39 +0700

    fix

--- BackendNailShop/Data/Entities/NailShopDbContext.cs
+++ BackendNailShop/Data/Entities/NailShopDbContext.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Data.Entities;
 
 public partial class NailShopDbContext : DbContext
 {
+    private readonly IConfiguration _configuration;
     public NailShopDbContext()
     {
     }
 
-    public NailShopDbContext(DbContextOptions<NailShopDbContext> options)
+    public NailShopDbContext(DbContextOptions<NailShopDbContext> options, IConfiguration configuration)
         : base(options)
     {
+        _configuration = configuration;
     }
 
     public virtual DbSet<Account> Accounts { get; set; }
@@ -40,8 +43,10 @@ public partial class NailShopDbContext : DbContext
     public virtual DbSet<Voucher> Vouchers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=NailShopDB;User Id=sa;Password=1;TrustServerCertificate=True;");
+    {
+        string connectionString = _configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
